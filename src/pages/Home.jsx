0b35b0758715dd07e7aa9d07c6ed8fc2b4c2d325
Rejane Raquel <rejane.lima87@gmail.com>
2022-09-08 19:32:06 -0300

    Requisito 05 Completo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 import Categories from './Categories';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: '',
+      searchValue: '',
+    };
+  }
+
+  fetchProducts = (url) => {
+    getProductsFromCategoryAndQuery(url).then((data) => this.setState({
+      products: data.results,
+      searchValue: '',
+    }));
+  };
+
+  searchProduct = ({ target }) => {
+    const { value } = target;
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=${value}`;
+    this.fetchProducts(url);
+  };
+
   render() {
+    const { products, searchValue } = this.state;
     return (
       <>
-        <Categories />
         <input type="text" />
         <p
           data-testid="home-initial-message"
@@ -15,14 +37,26 @@ class Home extends React.Component {
         </p>
         <input
           data-testid="query-input"
+          placeholder="Digite o nome do produto"
+          value={ searchValue }
+          onChange={ ({ target }) => this.setState({ searchValue: target.value }) }
         />
         <button
           data-testid="query-button"
           type="submit"
+          value={ searchValue }
+          onClick={ this.searchProduct }
         >
           Pesquisar
 
         </button>
+        <div>
+          { products ? products.map((product) => (
+            <div data-testid="product" key={ product.id } />
+
+          )) : <h3>Nenhum produto foi encontrado</h3>}
+        </div>
+        <Categories />
       </>
     );
   }
