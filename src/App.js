73a2e1fb7f7743fedc649edfb58719d267c58e57
miranda <magui@duck.com>
2022-09-08 16:33:41 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
+          <Link
+            data-testid="shopping-cart-button"
+            to="/cart"
+          >
+            Carrinho
+          </Link>
           <Switch>
             <Route exact path="/" component={ Home } />
+            <Route path="/cart" component={ ShoppingCart } />
           </Switch>
         </BrowserRouter>
       </header>
