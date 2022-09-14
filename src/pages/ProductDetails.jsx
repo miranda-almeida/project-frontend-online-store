a5e1d7217457c136deaeef48e1bc00adf47f4193
miranda-almeida <magui@duck.com>
2022-09-14 14:41:56 -0300

    requisito 9

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,6 +6,7 @@ import CartBtn from '../components/CartBtn';
 export default class ProductDetails extends Component {
   state = {
     product: '',
+    localStorageList: [],
   };
 
   componentDidMount() {
@@ -16,6 +17,36 @@ export default class ProductDetails extends Component {
     const { match: { params: { id } } } = this.props;
     const product = await getProductFromProductId(id);
     this.setState({ product });
+  };
+
+  addingToCart = (product) => {
+    product.quantity = 1;
+    const local = localStorage.getItem('cart');
+    const ifTrue = local ? JSON.parse(local) : [];
+    const verify = ifTrue.some((item) => item.id === product.id);
+
+    if (verify === true) {
+      const testing = ifTrue
+        .reduce((accumulator, current) => {
+          if (current.id === product.id) {
+            current.quantity += 1;
+            console.log(current.quantity);
+            accumulator = ([...accumulator, current]);
+            return accumulator;
+          }
+          accumulator = ([...accumulator, current]);
+          return accumulator;
+        }, []);
+      console.log(testing);
+      return localStorage.setItem('cart', JSON.stringify(testing));
+    }
+
+    const returnedList = [...ifTrue, product];
+    localStorage.setItem('cart', JSON.stringify(returnedList));
+    this.setState((previousState) => (
+      { localStorageList: [...previousState.localStorageList,
+        product] }
+    ));
   };
 
   render() {
@@ -32,6 +63,13 @@ export default class ProductDetails extends Component {
         <div>
           <CartBtn data-testid="shopping-cart-button" />
         </div>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ () => this.addingToCart(product) }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
